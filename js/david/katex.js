77c6fd8acd19299e92b04a2d4f4b7522dee1f1ea
Kozova1 <mug66kk@gmail.com>
2020-09-21 14:30:03 +0300

    modified js/david/katex.js

--- js/david/katex.js
+++ js/david/katex.js
@@ -8,11 +8,13 @@ const loadKatexBtn = document.getElementById('katex_load_btn');
 
 async function addKatex() {
     const katex = await import('https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.mjs');
+    console.log('imported');
 
     katexIn.addEventListener('click', () => {
-        katex.render(katexIn.value, katexOut, {
+        katex.default.render(katexIn.value, katexOut, {
             throwOnError: true
         });
+        console.log('compiled!');
     });
     loadKatexBtn.disabled = true;
     katexBtn.disabled = false;
