b6f64e3c4fa01838782289d3a430819bc75e4b78
Kozova1 <mug66kk@gmail.com>
2020-09-21 14:09:49 +0300

    added LOAD button to the katex compiler

--- js/david/katex.js
+++ js/david/katex.js
@@ -1,12 +1,19 @@
 'use strict';
-import katex from 'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.mjs';
 
 const katexIn = document.getElementById('katex_in');
 const katexOut = document.getElementById('katex_out');
 const katexBtn = document.getElementById('katex_btn');
+const loadKatexBtn = document.getElementById('katex_load_btn');
 
-katexBtn.addEventListener('click', () => {
-    katex.render(katexIn.value, katexOut, {
-        throwOnError: true
+
+loadKatexBtn.addEventListener('click', async () => {
+    const katex = await import('https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.mjs');
+
+    katexIn.addEventListener('click', () => {
+        katex.render(katexIn.value, katexOut, {
+            throwOnError: true
+        });
     });
+    loadKatexBtn.disabled = true;
+    katexBtn.disabled = false;
 });
