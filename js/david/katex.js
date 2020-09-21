bbf278af8d2e2f05289af1fb2a836794b437058d
Kozova1 <mug66kk@gmail.com>
2020-09-21 14:32:03 +0300

    reverted HTML Load button for KaTeX

--- js/david/katex.js
+++ js/david/katex.js
@@ -1,23 +1,12 @@
 'use strict';
+import katex from 'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.mjs';
 
 const katexIn = document.getElementById('katex_in');
 const katexOut = document.getElementById('katex_out');
 const katexBtn = document.getElementById('katex_btn');
-const loadKatexBtn = document.getElementById('katex_load_btn');
 
-
-async function addKatex() {
-    const katex = await import('https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.mjs');
-    console.log('imported');
-
-    katexIn.addEventListener('click', () => {
-        katex.default.render(katexIn.value, katexOut, {
-            throwOnError: true
-        });
-        console.log('compiled!');
+katexBtn.addEventListener('click', () => {
+    katex.render(katexIn.value, katexOut, {
+        throwOnError: true
     });
-    loadKatexBtn.disabled = true;
-    katexBtn.disabled = false;
-}
-
-loadKatexBtn.addEventListener('click', addKatex);
+});
